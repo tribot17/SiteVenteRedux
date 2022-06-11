9e03ae88ab3d34cf9f9848ea9906a125c109d6bb
Tristan | Tribot17 <tribot17@gmail.com>
2022-06-11 02:18:22 +0200

    update

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -6,22 +6,33 @@ import { addToCart } from "../redux/redux";
 
 const ProductPage = () => {
   const [productData, setProductData] = useState();
+  const [size, setSize] = useState();
   let { slug } = useParams();
   const dispatch = useDispatch();
 
   const index = slug.split("-");
   useEffect(() => {
-    if (index[0] == "homme") {
+    if (index[0] == "homme")
       setProductData(hommeProduct.filter((n) => n.id == index[1]));
-    }
   }, []);
 
   return (
-    <div>
+    <div className="displayProductPage">
       {productData && (
-        <div>
+        <div className="productContainer">
           <img src={productData[0].img} alt="" />
           <h1>{productData[0].name}</h1>
+          <select onChange={(e) => setSize(e.target.value)}>
+            <option>38</option>
+            <option>39</option>
+            <option>40</option>
+            <option>41</option>
+            <option>42</option>
+            <option>43</option>
+            <option>44</option>
+            <option>45</option>
+            <option>46</option>
+          </select>
           <button onClick={() => dispatch(addToCart(index[1]))}>
             Ajouter au panier
           </button>
